f80f6ac4bc032a90d6af1ccefa3403175cfdae03
mansikulkarni96 <mansikulkarni@localhost.localdomain>
2020-02-03 21:17:11 -0500

    Added go file and a dockerfile to run program in a container

---
+++ mankulka/hello_openshift.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// HelloWorld should have a comment documenting it.
+func main() {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+
+	fmt.Println("Hello Openshift!")
+}
